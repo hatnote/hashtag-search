963fd8a21681d981cbc566e6f7630d9592f32483
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-04-19 08:47:53 +0000

    lots of changes. redirect stdout and stderr for flup. still WIP on the all-hashtag search.

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+import os
+import sys
+from os.path import join as pjoin
+
 from flup.server.fcgi import WSGIServer
 from clastic import Application, render_json, render_basic
 from clastic.meta import MetaApplication
 from clastic.render import AshesRenderFactory
+from clastic.static import StaticApplication
 import oursql
-import os
+
 
 DB_CONFIG_PATH = os.path.expanduser('~/replica.my.cnf')
+FLUP_LOG_DIR = os.path.expanduser('~')
 TEMPLATES_PATH = 'templates'
+STATIC_PATH = 'static'
 DEFAULT_LANG = 'en'
 DEFAULT_DAYS = 3
+
+_CUR_PATH = os.path.dirname(__file__)
 
 
 def get_hashtags(tag, lang=DEFAULT_LANG, days=DEFAULT_DAYS):
@@ -28,7 +37,11 @@ def get_hashtags(tag, lang=DEFAULT_LANG, days=DEFAULT_DAYS):
                                 read_default_file=DB_CONFIG_PATH,
                                 charset=None)
     cursor = connection.cursor(oursql.DictCursor)
-    cursor.execute(query, (days, '(^| )[#]{1}' + tag + '[[:>:]]'))
+    if tag is None:
+        tag_pattern = '(^| )#[[:alpha:]]{1}[[:alnum:]]*[[:>:]]'
+    else:
+        tag_pattern = '(^| )[#]{1}%s[[:>:]]' % tag
+    cursor.execute(query, (days, tag_pattern))
     ret = cursor.fetchall()
     return ret
 
@@ -37,30 +50,39 @@ def process_revs(rev, lang):
     url_str = '%s.wikipedia.org/wiki/?diff=%s&oldid=%s'
     rev['diff_size'] = rev['rc_new_len'] - rev['rc_old_len']
     rev['date'] = rev['rc_timestamp']  # TODO
-    rev['diff_url'] = url_str % (rev['lang'],
+    rev['diff_url'] = url_str % (lang,
                                  rev['rc_this_oldid'],
                                  rev['rc_last_oldid'])
     return rev
 
 
-def generate_report(tag, lang=DEFAULT_LANG, days=DEFAULT_DAYS):
+def generate_report(tag=None, lang=DEFAULT_LANG, days=DEFAULT_DAYS):
     revs = get_hashtags(tag, lang, days)
     ret = [process_revs(rev, lang) for rev in revs]
+    if tag is None:
+        tag = 'all tags'
     return {'revisions': ret, 'tag': tag}
 
 
 def create_app():
-    templater = AshesRenderFactory(TEMPLATES_PATH)
-    routes = [('/<tag>', generate_report, 'design.html'),
+    _template_dir = os.path.join(_CUR_PATH, TEMPLATES_PATH)
+    _static_dir = os.path.join(_CUR_PATH, STATIC_PATH)
+    templater = AshesRenderFactory(_template_dir)
+    routes = [('/', generate_report, 'design.html'),
+              ('/<tag>', generate_report, 'design.html'),
               ('/get/<tag>', get_hashtags, render_json),
               ('/get/<tag>/<lang>', get_hashtags, render_json),
               ('/get/<tag>/<lang>/<days>', get_hashtags, render_json),
-              ('/meta', MetaApplication()),
-              ('/_dump_environ', lambda request: request.environ, templater)]
-    return Application(routes)
+              ('/static', StaticApplication(_static_dir)),
+              ('/meta', MetaApplication())]
+    return Application(routes, render_factory=templater)
 
 
 if __name__ == '__main__':
     wsgi_app = create_app()
+    # we have to redirect stdout and stderr, otherwise we see no
+    # errors beyond this point.
+    sys.stdout = open(pjoin(FLUP_LOG_DIR, 'flup-stdout.log'), 'a')
+    sys.stderr = open(pjoin(FLUP_LOG_DIR, 'flup-stderr.log'), 'a')
     wsgi_server = WSGIServer(wsgi_app)
     wsgi_server.run()
