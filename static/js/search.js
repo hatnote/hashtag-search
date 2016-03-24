73e327c1b526e7c5c44a37d1741d9c7a6363f7a6
Stephen LaPorte <stephen.laporte@gmail.com>
2016-03-24 07:03:23 +0000

    fixing search

--- static/js/search.js
+++ static/js/search.js
@@ -5,6 +5,9 @@ $(function() {
             lang = '?lang=' + lang;
         }
         var tag = $('#search').val();
+	if (tag.indexOf('#') == 0) {
+	    tag = tag.substring(1, tag.length);
+	}
         window.location.href = '/hashtags/search/' + tag + lang;
         e.preventDefault();
     });
